4ba8170ce746a529284ca8f269de6f2f9cd4230f
Nikita <nikitabuldakov@mail.ru>
2022-09-13 03:55:39 +0300

    Realese on PyPI

--- setup.py
+++ setup.py
@@ -1,23 +1,16 @@
 import setuptools
 
-with open("README.md") as file:
-    read_me_description = file.read()
-
 setuptools.setup(
-    name="DAO-module",
-    version="1.0",
+    name="DAO_module",
+    version="1.2.1.1",
     author="BuldakovN",
     author_email="nikitabuldakov@mail.ru",
     description="Модуль для работы с удаленной БД",
-    long_description=read_me_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/SADT-Boting/DAO-Module",
-    packages=['DAO'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    packages=['dao_module'],
     python_requires='>=3.5',
-    install_requires=["pymysql"]
+    install_requires = [
+        "PyMySQL >= 1.0",
+        "pymysql >= 1.0"
+    ],
 )
